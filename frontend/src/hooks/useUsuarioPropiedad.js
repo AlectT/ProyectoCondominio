5841442908f6420113a9e5d207e79881a51b648c
Noé Alexander Alvarado Hernández <nalvaradoh@miumg.edu.gt>
2026-05-13 11:52:55 -0600

    Fix import path for usuariosPropiedadApi

--- frontend/src/hooks/useUsuarioPropiedad.js
+++ frontend/src/hooks/useUsuarioPropiedad.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { usuariosPropiedadApi } from '../Api/usuarioPropiedadApi';
+import { usuariosPropiedadApi } from '../api/usuarioPropiedadApi';
 
 export function useUsuarioPropiedad() {
 	const [up, setUP] = useState([]);
