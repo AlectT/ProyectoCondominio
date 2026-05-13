6fad663bdfcd992b190568b70c5a13f7a03b0b1c
NoeAlvarado <nalvaradoh2002@gmail.com>
2026-05-13 12:48:53 -0600

    feat: Preparativos para el despliegue

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -1,23 +1,16 @@
 import cors from 'cors';
+import { IS_PRODUCTION, CORS_ORIGINS } from '../config/config.js';
 
-const origenesAceptados = [
-	'http://localhost:8080',
-	'http://localhost:1234',
-	'http://localhost:3000',
-	'http://localhost:5173',
-	'http://localhost:1000',
-	'http://192.168.140.138:5173',
-	'http://10.26.177.19:5173',
-	'http://192.168.0.9:5173',
-];
-
-export const middlewareCors = ({ origenes = origenesAceptados } = {}) =>
+export const middlewareCors = ({ origenes = CORS_ORIGINS } = {}) =>
 	cors({
 		origin: (origen, callback) => {
-			if (origenes.includes(origen)) {
-				return callback(null, true);
-			}
 			if (!origen) {
+				return callback(
+					IS_PRODUCTION ? new Error('Origen no permitido por CORS') : null,
+					!IS_PRODUCTION,
+				);
+			}
+			if (origenes.includes(origen)) {
 				return callback(null, true);
 			}
 			return callback(new Error('Origen no permitido por CORS'));
